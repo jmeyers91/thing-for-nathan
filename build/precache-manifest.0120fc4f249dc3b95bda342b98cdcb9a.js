a6f1ef10d5909db9b3605fc690970b0bbb2f95a8
James Meyers <james.meyers@emberex.com>
2019-04-07 13:56:06 -0700

    Add build to repo.

---
+++ build/precache-manifest.0120fc4f249dc3b95bda342b98cdcb9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f4a9c1f52cf4cef2e48",
+    "url": "/static/js/main.2c38f2a1.chunk.js"
+  },
+  {
+    "revision": "24d805f1c23138838ce9",
+    "url": "/static/js/2.622f6809.chunk.js"
+  },
+  {
+    "revision": "8f4a9c1f52cf4cef2e48",
+    "url": "/static/css/main.e42fc32f.chunk.css"
+  },
+  {
+    "revision": "4fdfb769535838e45c710db7ed7e4109",
+    "url": "/index.html"
+  }
+];
